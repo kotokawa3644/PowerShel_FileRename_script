6123e2b4d43a7f6a66dad7f9186d6e69995b5486
kotokawa <rakuten@kotokawa.com>
2025-12-30 15:39:52 +0900

    20251230_更新版

--- start_watch.bat
+++ start_watch.bat
@@ -1,2 +1,2 @@
 @echo off
-pwsh -NoExit -ExecutionPolicy Bypass -File "Z:\powershell\watch_polling_final_5.ps1"
+pwsh -NoExit -ExecutionPolicy Bypass -File "Z:\powershell\watch_polling_final_7-5.ps1"
